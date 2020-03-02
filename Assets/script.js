d249a115b5d546ae210e42f3695755256be729d7
rodriguezh21 <rodriguezh21@gmail.com>
2020-03-02 00:45:36 -0600

    Added timer to js and more css styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,50 +1,90 @@
 $(document).ready(function(){
+    $("#correct-answer").hide()
+    $("#wrong-answer").hide()
+    $("#timer").hide()
+    $("#highscore").hide()
+
     $('.first').hide();
     $('.second').hide();
     $('.third').hide();
     $('.fourth').hide();
+    $('#input').hide()
 
-    $(".paragraph .start-btn ").click(function(){
-        $(this).parents(".paragraph").hide();
+    var timeLeft = 60;
+    var elem = document.getElementById('timer');
+    var penatlyTime = 10 * 1000;
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft <= 0) {
+        clearTimeout(timerId);
+        
+      } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+      }
+    }  
+    
+
+    $(".intro .start-btn ").click(function(){
+        $(this).parents(".intro").hide();
         $(".first").show()
+        $("#timer").show()
     });
 
     $(".first-c").on("click", function() {
-        alert("Correct!");
         $(".first").hide();
-        $(".second").show()
+        $(".second").show();
+        $("#correct-answer").slideDown(400).delay(1000).fadeOut(400);
+        
+        
     });
 
     $(".first-a, .first-b, .first-d").on("click", function() {
-        alert("Wrong, try again!")
+        $("#wrong-answer").slideDown(400).delay(1000).fadeOut(400);
+        $("#wrong-answer").on("click", function(){
+           $("#timer").timeLeft -= penatlyTime;
+        })
+        
     })
 
     $(".second-d").on("click", function() {
-        alert("Correct!");
         $(".second").hide();
-        $(".third").show()
+        $(".third").show();
+    $("#correct-answer").slideDown(400).delay(1000).fadeOut(400);        
     });
+
     $(".second-a, .second-b, .second-c").on("click", function() {
-        alert("Wrong, try again!")
+        $("#wrong-answer").slideDown(400).delay(1000).fadeOut(400);
     })
 
     $(".third-c").on("click", function() {
-        alert("Correct!");
         $(".third").hide();
         $(".fourth").show()
+        $("#correct-answer").slideDown(400).delay(1000).fadeOut(400);        
     });
     $(".third-a, .third-b, .third-d").on("click", function() {
-        alert("Wrong, try again!")
+        $("#wrong-answer").slideDown(400).delay(1000).fadeOut(400);
     })
 
     $(".fourth-a").on("click", function() {
-        alert("Correct!");
         $(".fourth").hide();
-        $(".input").show()
+        $("#correct-answer").slideDown(400).delay(1000).fadeOut(400);
+        $("#timer").hide()
+        $("#input").show()
+        
     });
     $(".fourth-b, .fourth-c, .fourth-d").on("click", function() {
-        alert("Wrong, try again!")
+        $("#wrong-answer").slideDown(400).delay(1000).fadeOut(400);
     })
+    
+    $("#submit-btn").on("click", function(){
+        $("#input").hide()
+        $("#highscore").show()
+        $("#text").readOnly;
+        $("#text").show();
+
+    })   
     
 
     
